42ad054efa8f44d8d743106b0d1141f0480743b8
zhanghongbin01 <zhanghongbin01@baidu.com>
2017-08-21 12:52:14 +0800

    add demo

---
+++ linuxc/demo32.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+
+int main() {
+    cout << "Please input two numbers:" << endl;
+    int v1, v2;
+    cin >> v1 >> v2;
+    cout << "v1=" << v1 << " and " << "v2=" << v2 << endl;
+    cout << "v1 + v2 = " << v1 + v2 << endl;
+
+    cout << "get input,thanks,bye bye" << endl;
+
+    return 0;
+}
